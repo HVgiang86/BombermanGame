a53dfb6cad66dcc4729e47f5d8279c75a83a07c3
Hoang Giang <HVGiang86@gmail.com>
2022-11-11 10:52:02 +0700

    initial project

---
+++ src/main/java/bomberman/Bomberman.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bomberman;
+
+/**
+ * @author HVGiang86
+ *
+ */
+public class Bomberman {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
