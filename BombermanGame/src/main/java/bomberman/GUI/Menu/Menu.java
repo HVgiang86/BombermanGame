50d3aaad3658adbe8b4f92fef283a539e7e9056d
Tran Diem <diemtrantran0102@gmail.com>
2022-12-17 15:23:44 +0700

    model

--- BombermanGame/src/main/java/bomberman/GUI/Menu/Menu.java
+++ BombermanGame/src/main/java/bomberman/GUI/Menu/Menu.java
@@ -2,13 +2,13 @@ package bomberman.GUI.Menu;
 
 import javax.swing.JMenuBar;
 
-import bomberman.Games;
+
 import bomberman.Exceptions.LoadLevelException;
 import bomberman.GUI.Frame;
 
 public class Menu extends JMenuBar{
 	 public Menu(Frame frame) throws LoadLevelException {
-	        add(new Games(frame));
+	        add(new Game(frame));
 	        add(new Player(frame));
 	        add(new Level(frame));
 	        add(new Options(frame));
